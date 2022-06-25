eab1aac8b5341a8e76b0b71891e96879999e961e
subhalaxmi <beherasubhalaxmi999@gmail.com>
2022-06-25 19:00:13 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+import javax.sql.rowset.spi.SyncResolver;
+import javax.swing.event.SwingPropertyChangeSupport;
+
+class A{
+    public static void main(String[] args) {
+    int a=10;
+    int b=20; 
+    int c= a+b;
+    System.out.println(c);
+    }
+    
+    
+}
